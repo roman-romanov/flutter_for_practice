080107330dfc96011b0ca6b23d86af8f8ef4a16f
Roman <designer_79@mail.ru>
2021-09-13 00:21:23 +0300

    json_serialization

--- lib/json/address.dart
+++ lib/json/address.dart
@@ -1,3 +1,7 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'address.g.dart';
+
+@JsonSerializable()
 class Address {
   final String city;
   final String street;
@@ -10,20 +14,26 @@ class Address {
     required this.flat,
   });
 
-  factory Address.fromJson(Map<String, dynamic> json) {
-    return Address(
-      city: json['city'] as String,
-      street: json['street'] as String,
-      house: json['house'] as String,
-      flat: json['flat'] as int,
-    );
-  }
-  Map<String, dynamic> toJson() {
-    return {
-      'city': city,
-      'street': street,
-      'house': house,
-      'flat': flat,
-    };
-  }
+  factory Address.fromJson(Map<String, dynamic> json) =>
+      _$AddressFromJson(json);
+
+  // factory Address.fromJson(Map<String, dynamic> json) {
+  //   return Address(
+  //     city: json['city'] as String,
+  //     street: json['street'] as String,
+  //     house: json['house'] as String,
+  //     flat: json['flat'] as int,
+  //   );
+  // }
+
+  Map<String, dynamic> toJson() => _$AddressToJson(this);
+
+  // Map<String, dynamic> toJson() {
+  //   return {
+  //     'city': city,
+  //     'street': street,
+  //     'house': house,
+  //     'flat': flat,
+  //   };
+  // }
 }
